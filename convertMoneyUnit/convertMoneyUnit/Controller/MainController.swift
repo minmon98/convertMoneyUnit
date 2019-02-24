94fcaa53e4d46d068e520ac906c9bd91b37de6f8
minmon98 <40994028+minmon98@users.noreply.github.com>
2019-02-24 17:01:52 +0700

    Initialize Application

---
+++ convertMoneyUnit/convertMoneyUnit/Controller/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  convertMoneyUnit
+//
+//  Created by Minh Mon on 2/24/19.
+//  Copyright © 2019 Minh Mon. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
